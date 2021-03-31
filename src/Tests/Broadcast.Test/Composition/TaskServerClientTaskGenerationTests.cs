d7a75315f8194c2834c6914b49b6c1fcd410967a
Christian Walpen <christianwalpen@opacc.ch>
2021-03-31 22:22:49 +0200

    Moved integrationtests

--- src/Tests/Broadcast.Test/Composition/TaskServerClientTaskGenerationTests.cs
+++ src/Tests/Broadcast.Test/Composition/TaskServerClientTaskGenerationTests.cs
@@ -12,46 +12,6 @@ namespace Broadcast.Test.Composition
 	public class TaskServerClientTaskGenerationTests
 	{
 		[Test]
-		public void TaskServerClient_TaskGeneration_Send()
-		{
-			Broadcaster.Setup(s => { });
-
-			// execute a static method
-			// serializeable
-			TaskServerClient.Send(() => Trace.WriteLine("test"));
-
-			Assert.IsAssignableFrom<ExpressionTask>(Broadcaster.Server.GetStore().Single());
-		}
-
-		[Test]
-		public void TaskServerClient_TaskGeneration_Schedule()
-		{
-			Broadcaster.Setup(s => { });
-
-			// execute a static method
-			// serializeable
-			TaskServerClient.Schedule(() => Trace.WriteLine("test"), TimeSpan.FromSeconds(0.5));
-
-			Task.Delay(1000).Wait();
-
-			Assert.IsAssignableFrom<ExpressionTask>(Broadcaster.Server.GetStore().Single());
-		}
-
-		[Test]
-		public void TaskServerClient_TaskGeneration_Recurring()
-		{
-			Broadcaster.Setup(s => { });
-
-			// execute a static method
-			// serializeable
-			TaskServerClient.Recurring(() => Trace.WriteLine("test"), TimeSpan.FromSeconds(0.5));
-
-			Task.Delay(1000).Wait();
-
-			Assert.IsAssignableFrom<ExpressionTask>(Broadcaster.Server.GetStore().First());
-		}
-
-		[Test]
 		public void TaskServerClient_TaskGeneration_Recurring_Id()
 		{
 			Broadcaster.Setup(s => { });
