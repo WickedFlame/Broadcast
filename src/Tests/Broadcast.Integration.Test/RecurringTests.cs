3d3f5319b692ed92dce93af6cb441cea84c608a6
Christian Walpen <christianwalpen@opacc.ch>
2021-03-31 22:14:37 +0200

    Moved test to integration

--- src/Tests/Broadcast.Integration.Test/RecurringTests.cs
+++ src/Tests/Broadcast.Integration.Test/RecurringTests.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Broadcast.Test
+namespace Broadcast.Integration.Test
 {
     [TestFixture]
     public class RecurringTests
