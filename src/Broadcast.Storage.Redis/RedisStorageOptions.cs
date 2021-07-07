a9e14df51e1db7e240b982d26c33fbd183ea9efb
Christian Walpen <christianwalpen@opacc.ch>
2021-07-07 20:53:57 +0200

    Add project for redis storage

---
+++ src/Broadcast.Storage.Redis/RedisStorageOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Broadcast.Storage.Redis
+{
+	public class RedisStorageOptions
+	{
+		public int Db { get; set; }
+	}
+}
