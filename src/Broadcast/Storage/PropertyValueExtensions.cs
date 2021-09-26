416f5f0263b1c5f226013db794cb6dda62e04415
Christian Walpen <christianwalpen@opacc.ch>
2021-09-26 21:49:31 +0200

    PropertyValue ToBool

---
+++ src/Broadcast/Storage/PropertyValueExtensions.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Broadcast.Storage
+{
+	/// <summary>
+	/// Extension helper methods for <see cref="PropertyValue"/> and objec
+	/// </summary>
+	public static class PropertyValueExtensions
+	{
+		/// <summary>
+		/// Convert a object to a bool
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public static bool ToBool(this object obj)
+		{
+			if (obj == null)
+			{
+				return false;
+			}
+
+			if (obj is bool b)
+			{
+				return b;
+			}
+
+			switch (obj.ToString().ToLower())
+			{
+				case "true":
+				case "1":
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
