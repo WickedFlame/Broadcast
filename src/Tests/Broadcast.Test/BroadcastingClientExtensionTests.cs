b5df1c1c05f783fd29a7f58020cfd0c54d07894e
Christian Walpen <christianwalpen@opacc.ch>
2021-11-06 17:44:02 +0100

    Refactored test to run on CI

--- src/Tests/Broadcast.Test/BroadcastingClientExtensionTests.cs
+++ src/Tests/Broadcast.Test/BroadcastingClientExtensionTests.cs
@@ -15,45 +15,45 @@ namespace Broadcast.Test
 		[Test]
 		public void BroadcastingClient_Recurring()
 		{
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => new Mock<ITaskStore>().Object);
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Recurring("Id", () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			client.Recurring("Id", () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Name == "Id" && t.State == TaskState.New && t.Time.Value.Seconds == 5)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Name == "Id" && t.State == TaskState.New && t.Time.Value.Seconds == 5)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Recurring_IsRecurring()
 		{
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => new Mock<ITaskStore>().Object);
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Recurring("Id", () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			client.Recurring("Id", () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.IsRecurring)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.IsRecurring)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Recurring_NoName()
 		{
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => new Mock<ITaskStore>().Object);
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			client.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.IsRecurring && t.Name == "Debug.WriteLine")), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.IsRecurring && t.Name == "Debug.WriteLine")), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Recurring_ReturnedId()
 		{
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => new Mock<ITaskStore>().Object);
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			var id = client.Object.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			var id = client.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Id == id)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Id == id)), Times.Once);
 		}
 
 		[Test]
@@ -62,82 +62,86 @@ namespace Broadcast.Test
 			var existing = new RecurringTask {ReferenceId = "existing"};
 			var store = new Mock<ITaskStore>();
 			store.Setup(x => x.Storage(It.IsAny<Func<IStorage, RecurringTask>>())).Returns(() => existing);
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => store.Object);
+			var client = new BroadcastingClient(store.Object);
 
-			var id = client.Object.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			var id = client.Recurring(null, () => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Id == id && t.Id == existing.ReferenceId)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Id == id && t.Id == existing.ReferenceId)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Schedule_Id()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			var id = client.Object.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			var id = client.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Id == id)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Id == id)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Schedule_Time()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			client.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Time.Value.Seconds == 5)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Time.Value.Seconds == 5)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Schedule_NotRecurring()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
+			client.Schedule(() => System.Diagnostics.Debug.WriteLine("Test"), TimeSpan.FromSeconds(5));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => !t.IsRecurring)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => !t.IsRecurring)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Send_Id()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			var id = client.Object.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
+			var id = client.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Id == id)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Id == id)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Send_NoTime()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
+			client.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => t.Time == null)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => t.Time == null)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_Send_NotRecurring()
 		{
-			var client = new Mock<IBroadcastingClient>();
+			var store = new Mock<ITaskStore>();
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
+			client.Send(() => System.Diagnostics.Debug.WriteLine("Test"));
 
-			client.Verify(exp => exp.Enqueue(It.Is<ITask>(t => !t.IsRecurring)), Times.Once);
+			store.Verify(exp => exp.Add(It.Is<ITask>(t => !t.IsRecurring)), Times.Once);
 		}
 
 		[Test]
 		public void BroadcastingClient_DeleteTask()
 		{
 			var store = new Mock<ITaskStore>();
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => store.Object);
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.DeleteTask("id");
+			client.DeleteTask("id");
 
 			store.Verify(exp => exp.Delete("id"), Times.Once);
 		}
@@ -146,10 +150,9 @@ namespace Broadcast.Test
 		public void BroadcastingClient_DeleteRecurringTask_NoExisting()
 		{
 			var store = new Mock<ITaskStore>();
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => store.Object);
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.DeleteRecurringTask("id");
+			client.DeleteRecurringTask("id");
 
 			store.Verify(exp => exp.Delete(It.IsAny<string>()), Times.Never);
 		}
@@ -161,10 +164,9 @@ namespace Broadcast.Test
 			var store = new Mock<ITaskStore>();
 			store.Setup(x => x.Storage(It.IsAny<Func<IStorage, RecurringTask>>())).Returns(() => existing);
 
-			var client = new Mock<IBroadcastingClient>();
-			client.Setup(exp => exp.Store).Returns(() => store.Object);
+			var client = new BroadcastingClient(store.Object);
 
-			client.Object.DeleteRecurringTask("id");
+			client.DeleteRecurringTask("id");
 
 			store.Verify(exp => exp.Delete(existing.ReferenceId), Times.Once);
 		}
