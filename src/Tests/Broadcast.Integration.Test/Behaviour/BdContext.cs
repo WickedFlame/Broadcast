42af579322e53a998f91d5844eb97942fd85b39b
Christian Walpen <christianwalpen@opacc.ch>
2021-11-07 08:30:19 +0100

    Behaviour tests for BackgroundTask Setup

---
+++ src/Tests/Broadcast.Integration.Test/Behaviour/BdContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Broadcast.Integration.Test.Behaviour
+{
+	public class BdContext
+	{
+		public ITaskStore Store { get; set; }
+
+		public Broadcaster Server { get; set; }
+
+		public void Wait()
+		{
+			System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+		}
+	}
+}
--- src/Tests/Broadcast.Integration.Test/Behaviour/SetupOrderTests.cs
+++ src/Tests/Broadcast.Integration.Test/Behaviour/SetupOrderTests.cs
@@ -70,17 +70,5 @@ namespace Broadcast.Integration.Test.Behaviour
 		{
 			context.Store.All(t => t.State == TaskState.Processed).Should().BeTrue();
 		}
-
-		public class BdContext
-		{
-			public ITaskStore Store { get; set; }
-
-			public Broadcaster Server { get; set; }
-
-			public void Wait()
-			{
-				System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(5)).Wait();
-			}
-		}
 	}
 }
